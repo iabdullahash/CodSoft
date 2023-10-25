97c032bdef264b4353e0a05fd012469e73da9210
Abdullah Ashraf <100797902+iabdullahash@users.noreply.github.com>
2023-10-25 23:41:51 +0500

    Add files via upload

--- Level 1 Task 1/Zenfit Landing Page/assets/js/script.js
+++ Level 1 Task 1/Zenfit Landing Page/assets/js/script.js
@@ -3,8 +3,8 @@ $('.owl-carousel').owlCarousel({
   loop: true,
   margin: 0,
   autoplay: true,
-  autoplayTimeout:3000,
-  autoplayHoverPause:true,
+  autoplayTimeout: 3000,
+  autoplayHoverPause: true,
   responsiveClass: true,
   responsive: {
     0: {
@@ -18,3 +18,14 @@ $('.owl-carousel').owlCarousel({
     }
   }
 });
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.querySelector(".header").style.top = "0";
+  } else {
+    document.querySelector(".header").style.top = "-80px";
+  }
+  prevScrollpos = currentScrollPos;
+} 
